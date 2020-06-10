8f0fada2a9e715b92e503e00f009c4e095beabb0
tevinbm <tevin_maxwell@cable.comcast.com>
2020-06-10 13:27:18 -0400

    queries

--- queries.sql
+++ queries.sql
@@ -5,4 +5,10 @@
 ## Part 3: Test it with SQL
 -- DROP TABLE jobs;
 ## Part 4: Test it with SQL
---return list of names and description
+--SELECT names,description
+  FROM skill
+  INNER JOIN job_skills ON skill.id = job_skills_id
+  WHERE job_skill.jobs_id IS NOT NULL
+  ORDER BY name ASC;
+
+
